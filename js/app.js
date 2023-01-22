6a41f4c06f98dd8412c4f917bc25af9ba4beb801
s.zychkov <zychkov.serhii@gmail.com>
2023-01-23 00:37:58 +0200

    Third commit finish

--- js/app.js
+++ js/app.js
@@ -14,3 +14,26 @@ const sliderMain = new Swiper('.slider-main', {
         }
     }
 })
+
+const sliderBg = new Swiper('.slider-bg', {
+    parallax: true,
+    centeredSlides: true,
+    spaceBetween: 60,
+    slidesPerView: 3.5,
+})
+
+sliderMain.controller.control = sliderBg
+
+document.querySelectorAll('.slider-item').forEach(item => {
+    item.addEventListener('click', event => {
+        item.classList.toggle('opened')
+    })
+})
+
+let desc = document.querySelector('.description')
+sliderMain.on('slideChange', e => {
+    sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+    document.querySelectorAll('.slider-item').forEach(item => {
+        item.classList.remove('opened')
+    })
+})
